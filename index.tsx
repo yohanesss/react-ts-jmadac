750a1f9a0e9b641f20cdae283d0548d34edff077
yohanesss <test27c@gmail.com>
2021-10-05 16:44:14 -0700

    1 file modified, 1 added

--- index.tsx
+++ index.tsx
@@ -3,29 +3,6 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-interface AppProps { }
-interface AppState {
-  name: string;
-}
-
-class App extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
+import App from './src/App';
 
 render(<App />, document.getElementById('root'));
