3191467eab00c7ba51284b63f250efeb8da82abd
yohanesss <test27c@gmail.com>
2021-10-05 17:00:46 -0700

    3 files modified, 1 added

--- index.tsx
+++ index.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import './style.css';
 
 import App from './src/App';
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { Container } from 'react-bootstrap';
+
+import * as S from './global.styled';
+
 const App = () => {
-  return <h1>test</h1>;
+  return (
+    <Container>
+      <S.MainTitle>test</S.MainTitle>
+    </Container>
+  );
 };
 
 export default App;
